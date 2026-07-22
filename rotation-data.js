cc33e380ddb8b02ef33876d65f10fdf202cdb4f4
takatomaeda729 <ta.maeda@mayass.com>
2026-07-22 11:28:32 +0900

    Update rotation settings from web

--- rotation-data.js
+++ rotation-data.js
@@ -21,7 +21,7 @@ const IMAGE_LIST = [
 ];
 
 const MAP_MASTER = {
-  storm_point: { nameJa: "ストームポイント", nameEn: "Storm Point", image: "images/storm_point.jpg" },
+  storm_point: { nameJa: "ストームポイント", nameEn: "Storm Point", image: "images/e_district2.jpg" },
   worlds_edge: { nameJa: "ワールズエッジ", nameEn: "World's Edge", image: "images/worlds_edge.png" },
   e_district: { nameJa: "Eディストリクト", nameEn: "E-District", image: "images/e_district.jpg" },
   olympus: { nameJa: "オリンパス", nameEn: "Olympus", image: "images/olympus.jpg" },
